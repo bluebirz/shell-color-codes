f5bd90e71a8e00dcbc5b158dbd3d976130e7c86c
bluebirz <9207891+bluebirz@users.noreply.github.com>
2023-08-07 20:45:40 +0200

    add sh

---
+++ py/main.py
@@ -0,0 +1,9 @@
+from .bash_colors import BashColors
+
+
+def do_print() -> None:
+    colors = []
+
+
+if __name__ == "__main__":
+    do_print()
---
+++ sh/main.sh
@@ -0,0 +1,9 @@
+source bash_colors.sh
+
+echo "$DARK_RED"hello "$LIGHT_BLUE"world"$RESET"
+echo ""
+echo $PAINT_GREEN_BOLD"Final result:"$RESET
+echo "\tMichael -$LIGHT_GRAY_STRIKE DROPPED.$RESET"
+echo "\tAngie   -$LIGHT_RED_UNDERLINE FAILED.$RESET"
+echo "\tSam     -$LIGHT_GREEN_BOLD PASSED.$RESET"
+echo "\tEdmond  -$LIGHT_ORANGE_ITALIC NEED MORE ROUNDS.$RESET"
